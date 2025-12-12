5a3e4d9c9b1bdfc183a1c315335ccb1a16806751
Gayashan Gamage <gayashan.randimagamage@gmail.com>
2025-12-12 17:46:30 +0530

    style index page

--- frontend/src/components/menubar.vue
+++ frontend/src/components/menubar.vue
@@ -1,11 +1,19 @@
 <template>
-    <div class="flex flex-row justify-between w-full max-w-7xl mx-auto py-4">
-        <RouterLink to="/">
-            <h1 class="font-black text-3xl">TODO List</h1>
-        </RouterLink>
-        <!-- <RouterLink to="/user">
-            <button class="border">User</button>
-        </RouterLink> -->
+    <div class="w-full bg-blue-600">
+        <div class="flex flex-row justify-between w-full max-w-7xl mx-auto py-4 text-white">
+            <RouterLink to="/">
+                <div class="flex flex-col gap-2">
+                    <div class="flex flex-row items-center gap-2">
+                        <span class="material-icons text-blue-300">add_task</span>
+                        <h1 class="font-black text-3xl">TODO List</h1>
+                    </div>
+                    <p class="font-bold text-sm">Stay organize and get things done</p>
+                </div>
+            </RouterLink>
+            <!-- <RouterLink to="/user">
+                <button class="border">User</button>
+            </RouterLink> -->
+        </div>
     </div>
 </template>
 
--- frontend/src/views/index.vue
+++ frontend/src/views/index.vue
@@ -8,32 +8,41 @@
         <div class="grid grid-cols-2 gap-6 w-full max-w-7xl mx-auto h-screen py-10">
 
             <!-- list new tasks -->
-            <div class="flex flex-col gap-4 w-full h-fit bg-gray-100 p-6 rounded-md border-2 border-gray-200">
+            <div class="flex flex-col gap-4 w-full h-fit bg-blue-50/60 p-6 rounded-md border-2 border-gray-200">
                 <h2 class="font-semibold text-xl">Create new items</h2>
-                <input type="text" class="border py-1 px-4 rounded-md border-gray-400 w-[75%]" placeholder="Title" v-model="title"> 
-                <textarea name="" id="" class="border w-full py-1 px-4 rounded-md border-gray-400" placeholder="Description" v-model="description"></textarea>
-                <button class="py-1 px-8 w-fit ml-auto rounded-md bg-black hover:bg-black/80 active:bg-black/80 text-white" @click="addTask">Add</button>
+                <input type="text" class="border py-1 px-4 rounded-md border-gray-300 w-[75%] bg-white" placeholder="Title" v-model="title"> 
+                <textarea name="" id="" class="border w-full py-1 px-4 rounded-md border-gray-300 bg-white" placeholder="Description" v-model="description"></textarea>
+                <button class="flex flex-row justify-center items-center gap-2 py-1 px-8 w-fit ml-auto rounded-md bg-blue-600 hover:bg-blue-600/80 active:bg-blue-600/70 text-white" @click="addTask">
+                    <span class="material-icons text-white">add</span>
+                    Add
+                </button>
             </div>
 
             <!-- available todo list -->
-            <div class="w-full h-full">
+            <div class="flex flex-col gap-10 w-full h-full bg-blue-50/60 p-6 rounded-md border-2 border-gray-200">
                 
                 <!-- tabs area -->
                 <div class="grid grid-cols-2 gap-2">
-                    <button class="py-2 rounded-l-md" :class="todoTab == true ? 'bg-black text-white' : 'bg-white text-black border'" @click="chnageTab('todo')">ToDo list</button>
-                    <button class="py-2 rounded-r-md"  :class="todoTab == false ? 'bg-black text-white' : 'bg-white text-black border'" @click="chnageTab('compleated')">Compleated list</button>
+                    <button class="flex flex-row justify-center items-center gap-2 py-2 rounded-l-md" :class="todoTab == true ? 'bg-blue-600 text-white' : 'bg-white text-blue-600 border'" @click="chnageTab('todo')">
+                        <span class="material-icons text-white">list</span>
+                        ToDo list
+                    </button>
+                    <button class="flex flex-row justify-center items-center gap-2 py-2 rounded-r-md"  :class="todoTab == false ? 'bg-blue-600 text-white' : 'bg-white text-blue-600 border'" @click="chnageTab('compleated')">
+                        <span class="material-icons text-white" :class="todoTab == false ? 'text-blue-600' : 'text-white'">check_circle</span>
+                        Compleated list
+                    </button>
                 </div>
 
                 <!-- to do list -->
-                <div class=" w-full h-full p-10" v-if="todoTab">
-                    
-                    <!-- loading effect -->
-                    <div class="w-full h-[10vh] border-2 border-gray-200 animate-pulse bg-gray-200/70 rounded-lg" v-if="loading"></div>
+                <div class=" w-full h-full" v-if="todoTab">
                     
                     <!-- not found message -->
-                    <div class="" v-if="dataNotAvailable">
-                        <h1 class="">No task available</h1>
-                        <p class="">Still there are no any task available for To-Do</p>
+                    <div class="flex flex-col gap-2" v-if="dataNotAvailable">
+                        <div class="flex felx-row gap-4 items-center">
+                            <span class="material-icons text-red-300">warning</span>
+                            <h1 class="font-black text-2xl">No task available</h1>
+                        </div>
+                        <p class="text-lg font-light">Still there are no any task available for To-Do</p>
                     </div>
     
                     <!-- error message -->
@@ -44,22 +53,36 @@
                     
                     <!-- todo list available -->
                     <div class="flex flex-col gap-4" v-if="dataAvailable">
-                        <div class="flex flex-row" v-for="items in taskstore.newTask">
-                            <p class="w-full">{{ items }}</p>
-                            <button class="border" @click="compleateTask(items)">compleat</button>
+                        <div class="flex flex-row bg-white p-6 rounded-md" v-for="items in taskstore.newTask">
+                            <div class="">
+                                <div class="">
+                                    <h5 class="font-semibold text-gray-400 text-xs">Title</h5>
+                                    <p class="text-lg">{{ items.title }}</p>
+                                </div>
+                                <div class="">
+                                    <h5 class="font-semibold text-gray-400 text-xs">Description</h5>
+                                    <p class="text-lg">{{ items.description }}</p>
+                                </div>
+                                <div class="">
+                                    <h5 class="font-semibold text-gray-400 text-xs">Created at</h5>
+                                    <p>{{ new Date(items.created_at).getFullYear() }}-{{ String(new Date(items.created_at).getMonth()).padStart(2, '0') }}-{{ String(new Date(items.created_at).getDate()).padStart(2, '0') }}</p>
+                                </div>
+                            </div>
+                            <button class="flex flex-row justify-center items-center gap-2 py-1 px-8 w-fit ml-auto rounded-md bg-blue-600 hover:bg-blue-600/80 active:bg-blue-600/70 text-white h-fit mt-auto" @click="compleateTask(items)">compleat</button>
                         </div>
                     </div>
                 </div>
 
                 <!-- compleated list -->
-                <div class=" w-full h-full p-10" v-if="!todoTab">
-                    <!-- loading effect -->
-                    <div class="w-full h-[10vh] border-2 border-gray-200 animate-pulse bg-gray-200/70 rounded-lg" v-if="loading"></div>
+                <div class=" w-full h-full" v-if="!todoTab">
                     
                     <!-- not found message -->
-                    <div class="" v-if="dataAvailable">
-                        <h1 class="">No task available</h1>
-                        <p class="">Still there are no any task available for To-Do</p>
+                    <div class="flex flex-col gap-2" v-if="dataNotAvailable">
+                        <div class="flex felx-row gap-4 items-center">
+                            <span class="material-icons text-red-300">warning</span>
+                            <h1 class="font-black text-2xl">No compleated tasks available</h1>
+                        </div>
+                        <p class="text-lg font-light">Still there are no any task available for To-Do</p>
                     </div>
     
                     <!-- error message -->
@@ -70,9 +93,22 @@
                     
                     <!-- todo list available -->
                     <div class="flex flex-col gap-4" v-if="dataAvailable">
-                        <div class="flex flex-row" v-for="items in taskstore.compleatedTasks">
-                            <p class="w-full">{{ items }}</p>
-                            <button class="border" @click="deleteTaks(items.id)">delete</button>
+                        <div class="flex flex-row bg-white p-6 rounded-md" v-for="items in taskstore.compleatedTasks">
+                            <div class="">
+                                <div class="">
+                                    <h5 class="font-semibold text-gray-400 text-xs">Title</h5>
+                                    <p class="text-lg">{{ items.title }}</p>
+                                </div>
+                                <div class="">
+                                    <h5 class="font-semibold text-gray-400 text-xs">Description</h5>
+                                    <p class="text-lg">{{ items.description }}</p>
+                                </div>
+                                <div class="">
+                                    <h5 class="font-semibold text-gray-400 text-xs">Created at</h5>
+                                    <p>{{ new Date(items.created_at).getFullYear() }}-{{ String(new Date(items.created_at).getMonth()).padStart(2, '0') }}-{{ String(new Date(items.created_at).getDate()).padStart(2, '0') }}</p>
+                                </div>
+                            </div>
+                            <button class="flex flex-row justify-center items-center gap-2 py-1 px-8 w-fit ml-auto rounded-md bg-red-400 hover:bg-red-400/80 active:bg-red-400/70 text-white h-fit mt-auto" @click="deleteTaks(items.id)">delete</button>
                         </div>
                     </div>
                 </div>
@@ -101,7 +137,6 @@ import Floatingmessage from '@/components/floatingmessage.vue';
 const taskstore = useTaskStore()
 
 // UI management states
-const loading = ref(false)
 const dataAvailable = ref(false) 
 const dataNotAvailable = ref(false) 
 const errorMessage = ref(false)
@@ -136,7 +171,7 @@ const addTask = () => {
 }
 
 const getTask = () => {
-    loading.value = true
+    // loading.value = true
     axios.get(`${import.meta.env.VITE_API_URL}/tasks/get`, 
         {
             params : {
@@ -145,7 +180,6 @@ const getTask = () => {
         }
     )
     .then((success) => {
-        loading.value = false
         taskstore.newTask = success.data
         if(taskstore.newTask.length !== 0){
             dataAvailable.value = true
@@ -154,12 +188,10 @@ const getTask = () => {
         }
     })
     .catch((error) => {
-        loading.value = false
         errorMessage.value = true
     })
 }
 const getCompleatedTask = () => {
-    loading.value = true
     axios.get(`${import.meta.env.VITE_API_URL}/tasks/get`, 
         {
             params : {
@@ -168,7 +200,6 @@ const getCompleatedTask = () => {
         }
     )
     .then((success) => {
-        loading.value = false
         taskstore.compleatedTasks = success.data
         if(taskstore.compleatedTasks.length !== 0){
             dataAvailable.value = true
@@ -177,7 +208,6 @@ const getCompleatedTask = () => {
         }
     })
     .catch((error) => {
-        loading.value = false
         errorMessage.value = true
     })
 }
@@ -223,8 +253,13 @@ const showToast = (title, description) => {
 }
 
 const chnageTab = (val) => {
+    dataAvailable.value = false
+    dataNotAvailable.value = false
+    errorMessage.value = false
+
     if(val == 'todo'){
         todoTab.value = true
+        getTask()
     }else if(val == 'compleated'){
         todoTab.value = false
         getCompleatedTask()
